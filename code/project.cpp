6caa46dc3ffe0193264bee9f34a23c26724df7b2
Andreas Stokkeland <andreas.stokkeland@hotmail.com>
2019-11-06 22:55:47 +0100

    Andreas

--- code/project.cpp
+++ code/project.cpp
@@ -12,7 +12,7 @@ void project() {
 	tempTrender t(pathToFile); //Instantiate your analysis object
 	
 	t.tempOnDay(10, 10);
-	t.MeanTempAndTempOnDate(1951,2,2);
+	t.MeanTempAndTempOnDate(1980,02,02);
 	
 }
 
